756bc3115fe8c69d714c5fab40bde8101dea7e37
Shegtrom17 <shegstrom17@gmail.com>
2025-10-05 14:40:14 -0500

    Update index.ts

--- supabase/functions/src/supabase/functions/create-referral/index.ts
+++ supabase/functions/src/supabase/functions/create-referral/index.ts
@@ -1,4 +1,4 @@
-// src/supabase/functions/create-referral/index.ts
+/create-referral/index.ts
 
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
